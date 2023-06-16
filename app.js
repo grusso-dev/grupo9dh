d813c0dcdec1eee2c9e48038e4401577e8248e56
UninstallUrPC <echikup@hotmail.com>
2023-06-15 22:17:44 -0300

    fix rapido appjs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, "/public")))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/"))
+  res.sendFile(path.join(__dirname, "/views/index.html"))
 })
 
 app.get('/checkout', (req, res) => {
