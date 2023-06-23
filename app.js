42194c883f6d624a4672f75a05f0ab17de468ce5
MarcosMena <marcosmenarivero@gmail.com>
2023-06-23 17:33:27 -0300

    Envío un commit a esta rama para ver si me deja hacer un pull de la rama orginal , no hay cambios específicos

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-app.use(express.static(path.join(__dirname, "/public")))
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"))
-})
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.get('/checkout', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/checkout.html"))
-})
+var app = express();
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/login.html"))
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/register.html"))
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/registerdata', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/registerdata.html"))
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.get('/detail', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/detail.html"))
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`Aplicacion escuchando en el puerto: ${port}`)
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
