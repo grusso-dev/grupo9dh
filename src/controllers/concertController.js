c49c60dbe7b55abd5683fef88fc7c813ccea36c1
Gustavo Russo <russo.gustavo@gmail.com>
2023-07-06 20:16:15 -0300

    Se afgrega basic-ft, Archivo data y se crea la conexion a ftp

--- src/controllers/concertController.js
+++ src/controllers/concertController.js
@@ -1,4 +1,28 @@
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const ftp = require('basic-ftp');
+
+const conserstFilePath = path.join(__dirname, '../data/consertsDataBase.json');
+const consets = JSON.parse(fs.readFileSync(consertsFilePath, 'utf-8'));
+
+async function ftp_upload(image_origin_rute, image_destiny_route){
+
+		// Connect to the FTP server
+		const client = new ftp.Client();
+		await client.access({
+		  host: 'ftp.linsock.com.ar', 
+		  user: 'u629722589.soundstage',
+		  password: '12_Soundstage_34'
+		});
+	
+		// Upload the file to the FTP server
+		await client.uploadFrom(image_origin_rute, image_destiny_route);
+	
+		// Close the FTP connection
+		client.close();
+
+}
+
 const rutasconcertController = {
 
   detail: (req, res) => {
@@ -9,7 +33,12 @@ const rutasconcertController = {
   },
   edit: (req, res) => {
     res.render("editconcert");
-  }
+  },
+  store: async (req, res) => {
+    await ftp_upload(	path.join(__dirname,'../../public/images/products', req.file.filename) , '/www/images/' + req.file.filename);
+    
+
+  },
 };
 
 module.exports = rutasconcertController;
