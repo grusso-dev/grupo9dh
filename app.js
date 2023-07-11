7b77fa3c75e956fb29fca1fd9ddcb92d06c251f7
Gustavo Russo <russo.gustavo@gmail.com>
2023-07-11 20:04:16 -0300

    Agrego json de usuarios y comienzo con el desarrollo del registro de usuario

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
 
 app.set("views", path.join(__dirname, "/src/views"))
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "/public")))
+app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 
 //const rutasProductos = require('./src/routes/productRoutes');//
 const rutasIndex = require('./src/routes/indexRoutes');
