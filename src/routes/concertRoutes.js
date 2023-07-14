9a32a636f4dcf61f19bc14cf3293883c0435b34a
Gustavo Russo <russo.gustavo@gmail.com>
2023-07-13 22:41:51 -0300

    Modificamos las rutas con Jose

--- src/routes/concertRoutes.js
+++ src/routes/concertRoutes.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 const concertController = require("../controllers/concertController");
 
-router.get('/', concertController.concerts);
-router.get('/:id', concertController.detail);
-router.get('/', concertController.create);
-router.post('/', concertController.create);
-router.get('/:id/edit', concertController.editConcert);
-router.put('/:id', concertController.editConcert);
-router.delete('/:id', concertController.deleteConcert);
+router.get('/', concertController.concerts);//listado
+//conciertos/detail/1
+router.get('/detail/:id', concertController.detail);//ver
+router.get('/create', concertController.create);//crear
+//router.post('/create', concertController.saveConcert);// Guardar
+router.get('/edit/:id', concertController.editConcert); // Mustra vista de edicion 
+router.put('/edit/:id', concertController.editConcert); // Guardar info editada
+router.delete('/delete/:id', concertController.deleteConcert); // Eliminar
 
 
 
 module.exports = router; 
+
