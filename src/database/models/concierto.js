d873019203be80c99414d8bb0772365ed369f21d
Victoria Azcarate <victoriaazc@gmail.com>
2023-09-02 19:30:17 -0300

    avazando con models y controller de concert, pero falta banda

---
+++ src/database/models/concierto.js
@@ -0,0 +1,63 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    
+    let alias = "Conciertos"; 
+
+    let cols = {
+        id: {
+            primaryKey: true,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+        },
+        user_id: {
+            type: DataTypes.INTEGER
+        },
+        sector_id: {
+            type: DataTypes.INTEGER
+        },
+        artista: {
+            type: DataTypes.STRING(30)
+        }, 
+        title: {
+            type: DataTypes.STRING(20)
+        } ,
+        date: {
+            type: DataTypes.DATE
+        },
+        direccion: {
+            type: DataTypes.STRING(100)
+        },
+        provincia: {
+            type: DataTypes.STRING(30)
+        },
+        ciudad: {
+            type: DataTypes.STRING(30)
+        },
+        image: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        } , 
+        descripcion: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        }
+
+    };
+
+    let config = {
+        tableName: 'conciertos',
+        timestamps: false
+    };
+    
+    const Concierto = sequelize.define(alias, cols, config);
+
+    Concierto.associate = function (models) {
+        Concierto.belongsTo(models.Generos, {
+            as: "generos",
+            foreignKey: "genre_id"
+        } )
+    }
+
+    return Concierto;
+}
