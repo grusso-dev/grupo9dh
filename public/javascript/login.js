674c0bfebb6c95fc4fe58bf4dd9314ee8b43d760
MarcosMena <marcosmenarivero@gmail.com>
2023-09-23 11:37:37 -0300

    login.js priemras validaciones

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,19 @@
 window.addEventListener("load", function(){
 let formulario = document.querySelectorAll("form.input-group")
+
+formulario.addEventListener('submit', function(e){
+   let email = document.querySelector("input.form-control")
+   if(email.value == ""){
+    alert("Debe completar el email")
+   }
+})
+
+formulario.addEventListener('submit', function(e){
+   let password = document.querySelector("input.form-control")
+   if(password.value == ""){
+    alert("Debe completar la contraseña")
+   }else if(password.value.length < 5){
+      alert("La contraseña es demasiado corta")
+   }
+})
 })
