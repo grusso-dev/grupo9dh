74fc43044a74ce2860e51838627b3dc169036d5c
Uninstall <echikup@hotmail.com>
2023-09-24 19:28:35 -0300

    Cambios a todos los conciertos y sus rutas

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
+const { Concert } = require('../database/models/conciertos');
+
 const indexController = require("../controllers/indexController")
 router.get('/', indexController.index);
 router.get('/login', indexController.login);
@@ -8,6 +10,15 @@ router.get('/checkout', indexController.checkout);
 router.get('/register', indexController.register);
 router.get('/detail/:id', indexController.detail);
 router.get('/registerdata', indexController.registerdata);
-router.get('/todosLosConciertos', indexController.todosLosConciertos);
+router.get('/todosLosConciertos', (req, res) => {
+    conciertos.findAll()
+      .then(concerts => {
+        res.render('todosLosConciertos', { conciertos });
+      })
+      .catch(error => {
+        console.error('Error', error);
+      });
+  });
+  
 
 module.exports = router 
